12d24f8867186a2c8ded6ef76a4d6071954f098d
IvanAndriWahyudi <ivanandri.w04@gmail.com>
2022-08-09 23:35:48 +0700

    awal

--- sc.js
+++ sc.js
@@ -1,2 +1,9 @@
-var nama = prompt("Hallo Siapa namamu = ");
-alert("halo " + nama);
+alert("Selamat Malam");
+var nama = prompt("Siapa namamu = ");
+var jam = prompt("Hai " + nama + " Kenapa Belum Tidur,, Sekarang sudah jam berapa = ");
+
+if(jam <= 12){
+    alert("Hari sudah malam Ayo tidur");
+}else if(jam <= 6){
+    alert("Hari sudah pagi");
+}
